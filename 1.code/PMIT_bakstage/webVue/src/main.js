d47f85153bf8eb29dd22ab315be2ce1be2978a20
r91046 <haitaoruan@163.com>
2018-11-24 09:30:23 +0800

    提交交互文档

--- 1.code/PMIT_bakstage/webVue/src/main.js
+++ 1.code/PMIT_bakstage/webVue/src/main.js
@@ -2,26 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import elmentui from 'element-ui'
-import VueI18n from "vue-i18n";
+import elmentui from 'element-ui';
+import 'normalize.css' // A modern alternative to CSS resets
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+import i18n from './lang' // Internationalization
+import './icons' // icon
 Vue.config.productionTip = false;
 Vue.use(elmentui)
-Vue.use(VueI18n)
-const i18n = new VueI18n({
-  locale: 'CN',    // 语言标识
-  messages: {
-    en: {
-      message: {
-        hello: 'hello world'
-      }
-    },
-    cn: {
-      message: {
-        hello: '你好、世界'
-      }
-    }
-  },
-})
 
 new Vue({
   i18n,
