442cd86de5d0ff0616d954af2f3870628ffa700f
wnj21 <734668541@qq.com>
2018-11-26 17:42:36 +0800

    个人信息基础类

---
+++ 01.Code/PMIT_bakstage/src/main/java/com/isoftstone/pmit/project/hrbp/entity/BaseStaffInfo.java
@@ -0,0 +1,480 @@
+package com.isoftstone.pmit.project.hrbp.entity;
+
+public class BaseStaffInfo {
+    private String employeeID;
+    private String employeeName;
+    private String idNumber;
+    private String isoftStatus;
+    private String personCategory;
+    private String ifBackBone;
+    private String ifOffShore;
+    private String workPlace;
+    private int workStationNumber;
+    private String cooperationMode;
+    private String positionRole;
+    private String skill;
+    private String sillDirection;
+    private String technologyPlatform;
+    private String technologyGrade;
+    private String position;
+    private String positionResponsibilities;
+    private String tutorName;
+    private String tutorId;
+    private String ifInTutorResourcePool;
+    private String directLeaderId;
+    private String directLeaderName;
+    private String hwId;
+    private String hwMailbox;
+    private String employmentDate;
+    private String departureDate;
+    private String generalPosition;
+    private String currentPositionDate;
+    private String costCentercostCenter;
+    private String bu;
+    private String du;
+    private String pdu;
+    private String workPlaceArea;
+    private String sex;
+    private String telephone;
+    private String graduationSchool;
+    private String graduationMajor;
+    private String graduationTime;
+    private String if211;
+    private String ifUnderGraduates;
+    private String ifAbvoveTwoInstitutions;
+    private String education;
+    private String degree;
+    private String ifGraduate;
+    private String educationMode;
+    private String projectTeamId;
+    private String currentStatus;
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getIsoftStatus() {
+        return isoftStatus;
+    }
+
+    public void setIsoftStatus(String isoftStatus) {
+        this.isoftStatus = isoftStatus;
+    }
+
+    public String getPersonCategory() {
+        return personCategory;
+    }
+
+    public void setPersonCategory(String personCategory) {
+        this.personCategory = personCategory;
+    }
+
+    public String getIfBackBone() {
+        return ifBackBone;
+    }
+
+    public void setIfBackBone(String ifBackBone) {
+        this.ifBackBone = ifBackBone;
+    }
+
+    public String getIfOffShore() {
+        return ifOffShore;
+    }
+
+    public void setIfOffShore(String ifOffShore) {
+        this.ifOffShore = ifOffShore;
+    }
+
+    public String getWorkPlace() {
+        return workPlace;
+    }
+
+    public void setWorkPlace(String workPlace) {
+        this.workPlace = workPlace;
+    }
+
+    public int getWorkStationNumber() {
+        return workStationNumber;
+    }
+
+    public void setWorkStationNumber(int workStationNumber) {
+        this.workStationNumber = workStationNumber;
+    }
+
+    public String getCooperationMode() {
+        return cooperationMode;
+    }
+
+    public void setCooperationMode(String cooperationMode) {
+        this.cooperationMode = cooperationMode;
+    }
+
+    public String getPositionRole() {
+        return positionRole;
+    }
+
+    public void setPositionRole(String positionRole) {
+        this.positionRole = positionRole;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getSillDirection() {
+        return sillDirection;
+    }
+
+    public void setSillDirection(String sillDirection) {
+        this.sillDirection = sillDirection;
+    }
+
+    public String getTechnologyPlatform() {
+        return technologyPlatform;
+    }
+
+    public void setTechnologyPlatform(String technologyPlatform) {
+        this.technologyPlatform = technologyPlatform;
+    }
+
+    public String getTechnologyGrade() {
+        return technologyGrade;
+    }
+
+    public void setTechnologyGrade(String technologyGrade) {
+        this.technologyGrade = technologyGrade;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPositionResponsibilities() {
+        return positionResponsibilities;
+    }
+
+    public void setPositionResponsibilities(String positionResponsibilities) {
+        this.positionResponsibilities = positionResponsibilities;
+    }
+
+    public String getTutorName() {
+        return tutorName;
+    }
+
+    public void setTutorName(String tutorName) {
+        this.tutorName = tutorName;
+    }
+
+    public String getTutorId() {
+        return tutorId;
+    }
+
+    public void setTutorId(String tutorId) {
+        this.tutorId = tutorId;
+    }
+
+    public String getIfInTutorResourcePool() {
+        return ifInTutorResourcePool;
+    }
+
+    public void setIfInTutorResourcePool(String ifInTutorResourcePool) {
+        this.ifInTutorResourcePool = ifInTutorResourcePool;
+    }
+
+    public String getDirectLeaderId() {
+        return directLeaderId;
+    }
+
+    public void setDirectLeaderId(String directLeaderId) {
+        this.directLeaderId = directLeaderId;
+    }
+
+    public String getDirectLeaderName() {
+        return directLeaderName;
+    }
+
+    public void setDirectLeaderName(String directLeaderName) {
+        this.directLeaderName = directLeaderName;
+    }
+
+    public String getHwId() {
+        return hwId;
+    }
+
+    public void setHwId(String hwId) {
+        this.hwId = hwId;
+    }
+
+    public String getHwMailbox() {
+        return hwMailbox;
+    }
+
+    public void setHwMailbox(String hwMailbox) {
+        this.hwMailbox = hwMailbox;
+    }
+
+    public String getEmploymentDate() {
+        return employmentDate;
+    }
+
+    public void setEmploymentDate(String employmentDate) {
+        this.employmentDate = employmentDate;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getGeneralPosition() {
+        return generalPosition;
+    }
+
+    public void setGeneralPosition(String generalPosition) {
+        this.generalPosition = generalPosition;
+    }
+
+    public String getCurrentPositionDate() {
+        return currentPositionDate;
+    }
+
+    public void setCurrentPositionDate(String currentPositionDate) {
+        this.currentPositionDate = currentPositionDate;
+    }
+
+    public String getCostCentercostCenter() {
+        return costCentercostCenter;
+    }
+
+    public void setCostCentercostCenter(String costCentercostCenter) {
+        this.costCentercostCenter = costCentercostCenter;
+    }
+
+    public String getBu() {
+        return bu;
+    }
+
+    public void setBu(String bu) {
+        this.bu = bu;
+    }
+
+    public String getDu() {
+        return du;
+    }
+
+    public void setDu(String du) {
+        this.du = du;
+    }
+
+    public String getPdu() {
+        return pdu;
+    }
+
+    public void setPdu(String pdu) {
+        this.pdu = pdu;
+    }
+
+    public String getWorkPlaceArea() {
+        return workPlaceArea;
+    }
+
+    public void setWorkPlaceArea(String workPlaceArea) {
+        this.workPlaceArea = workPlaceArea;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getGraduationSchool() {
+        return graduationSchool;
+    }
+
+    public void setGraduationSchool(String graduationSchool) {
+        this.graduationSchool = graduationSchool;
+    }
+
+    public String getGraduationMajor() {
+        return graduationMajor;
+    }
+
+    public void setGraduationMajor(String graduationMajor) {
+        this.graduationMajor = graduationMajor;
+    }
+
+    public String getGraduationTime() {
+        return graduationTime;
+    }
+
+    public void setGraduationTime(String graduationTime) {
+        this.graduationTime = graduationTime;
+    }
+
+    public String getIf211() {
+        return if211;
+    }
+
+    public void setIf211(String if211) {
+        this.if211 = if211;
+    }
+
+    public String getIfUnderGraduates() {
+        return ifUnderGraduates;
+    }
+
+    public void setIfUnderGraduates(String ifUnderGraduates) {
+        this.ifUnderGraduates = ifUnderGraduates;
+    }
+
+    public String getIfAbvoveTwoInstitutions() {
+        return ifAbvoveTwoInstitutions;
+    }
+
+    public void setIfAbvoveTwoInstitutions(String ifAbvoveTwoInstitutions) {
+        this.ifAbvoveTwoInstitutions = ifAbvoveTwoInstitutions;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getIfGraduate() {
+        return ifGraduate;
+    }
+
+    public void setIfGraduate(String ifGraduate) {
+        this.ifGraduate = ifGraduate;
+    }
+
+    public String getEducationMode() {
+        return educationMode;
+    }
+
+    public void setEducationMode(String educationMode) {
+        this.educationMode = educationMode;
+    }
+
+    public String getProjectTeamId() {
+        return projectTeamId;
+    }
+
+    public void setProjectTeamId(String projectTeamId) {
+        this.projectTeamId = projectTeamId;
+    }
+
+    public String getCurrentStatus() {
+        return currentStatus;
+    }
+
+    public void setCurrentStatus(String currentStatus) {
+        this.currentStatus = currentStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseStaffInfo{" +
+                "employeeID='" + employeeID + '\'' +
+                ", employeeName='" + employeeName + '\'' +
+                ", idNumber='" + idNumber + '\'' +
+                ", isoftStatus='" + isoftStatus + '\'' +
+                ", personCategory='" + personCategory + '\'' +
+                ", ifBackBone='" + ifBackBone + '\'' +
+                ", ifOffShore='" + ifOffShore + '\'' +
+                ", workPlace='" + workPlace + '\'' +
+                ", workStationNumber=" + workStationNumber +
+                ", cooperationMode='" + cooperationMode + '\'' +
+                ", positionRole='" + positionRole + '\'' +
+                ", skill='" + skill + '\'' +
+                ", sillDirection='" + sillDirection + '\'' +
+                ", technologyPlatform='" + technologyPlatform + '\'' +
+                ", technologyGrade='" + technologyGrade + '\'' +
+                ", position='" + position + '\'' +
+                ", positionResponsibilities='" + positionResponsibilities + '\'' +
+                ", tutorName='" + tutorName + '\'' +
+                ", tutorId='" + tutorId + '\'' +
+                ", ifInTutorResourcePool='" + ifInTutorResourcePool + '\'' +
+                ", directLeaderId='" + directLeaderId + '\'' +
+                ", directLeaderName='" + directLeaderName + '\'' +
+                ", hwId='" + hwId + '\'' +
+                ", hwMailbox='" + hwMailbox + '\'' +
+                ", employmentDate='" + employmentDate + '\'' +
+                ", departureDate='" + departureDate + '\'' +
+                ", generalPosition='" + generalPosition + '\'' +
+                ", currentPositionDate='" + currentPositionDate + '\'' +
+                ", costCentercostCenter='" + costCentercostCenter + '\'' +
+                ", bu='" + bu + '\'' +
+                ", du='" + du + '\'' +
+                ", pdu='" + pdu + '\'' +
+                ", workPlaceArea='" + workPlaceArea + '\'' +
+                ", sex='" + sex + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", graduationSchool='" + graduationSchool + '\'' +
+                ", graduationMajor='" + graduationMajor + '\'' +
+                ", graduationTime='" + graduationTime + '\'' +
+                ", if211='" + if211 + '\'' +
+                ", ifUnderGraduates='" + ifUnderGraduates + '\'' +
+                ", ifAbvoveTwoInstitutions='" + ifAbvoveTwoInstitutions + '\'' +
+                ", education='" + education + '\'' +
+                ", degree='" + degree + '\'' +
+                ", ifGraduate='" + ifGraduate + '\'' +
+                ", educationMode='" + educationMode + '\'' +
+                ", projectTeamId='" + projectTeamId + '\'' +
+                ", currentStatus='" + currentStatus + '\'' +
+                '}';
+    }
+}
