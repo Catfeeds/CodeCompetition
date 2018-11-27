2dcdad139f69b7c74a83a80db35468d6d39a6b8f
dgjsjf <15289340589@163.com>
2018-11-27 10:24:12 +0800

    更新数据字典: 增加员工系统角色表 增加建表sql : 员工系统角色表  系统角色表

--- 04.Doc/01.设计/骨干管理系统_路人甲_员工基本信息表.sql
+++ 04.Doc/01.设计/骨干管理系统_路人甲_员工基本信息表.sql
@@ -77,3 +77,47 @@ CREATE TABLE `base_staff_info` (
 -- Records of base_staff_info
 -- ----------------------------
 INSERT INTO `base_staff_info` VALUES ('146500', '薛景超', '610221199141185415', '在职', '实施类别', '否', '否', '环普3F', null, 'FP', '开发', 'java', 'java', null, null, '', null, null, null, '否', null, '', 'WX524416', 'xuejingchao@huawei.com', '2018/3/1', null, '中级工程师(二级)-HW', '2018/5/1', 'HW西安2012实施一部', '薛国明', '2012', '中央软件院', 'GENEX产品部', '西安', '男', '18792642388', null, null, null, null, null, null, null, null, null, null, null, '0');
+
+-- ----------------------------
+-- Table structure for hrbp_role
+-- ----------------------------
+DROP TABLE IF EXISTS `hrbp_role`;
+CREATE TABLE `hrbp_role` (
+  `role_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `role_name` varchar(30) NOT NULL COMMENT '角色名称',
+  `role_key` varchar(100) NOT NULL COMMENT '角色权限字符串',
+  `role_sort` int(4) NOT NULL COMMENT '显示顺序',
+  `data_scope` int(4) DEFAULT '1' COMMENT '数据范围（1：全部数据权限 2：自定数据权限）',
+  `status` int(4) NOT NULL COMMENT '角色状态（0正常 1停用）',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT NULL COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `description` varchar(500) DEFAULT NULL COMMENT '描述',
+  `remark` varchar(500) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='角色信息表';
+
+-- ----------------------------
+-- Records of hrbp_role
+-- ----------------------------
+INSERT INTO `hrbp_role` VALUES ('1', '管理员', 'admin', '1', '3', '1', 'admin', '2018-11-25 14:09:40', null, null, '系统设置与维护', null);
+INSERT INTO `hrbp_role` VALUES ('1', '管理员', 'main', '1', '3', '1', 'admin', '2018-11-25 14:09:40', null, null, '系统监控', null);
+INSERT INTO `hrbp_role` VALUES ('2', 'pm', 'pm', '1', '3', '1', 'admin', '2018-11-25 14:09:40', null, null, '项目组设置', null);
+INSERT INTO `hrbp_role` VALUES ('3', '信息录入员', 'person', '1', '1', '1', 'admin', '2018-11-25 14:11:45', null, null, '负责信息的上传', null);
+
+-- ----------------------------
+-- Table structure for hrbp_staffinfo_role
+-- ----------------------------
+DROP TABLE IF EXISTS `hrbp_staffinfo_role`;
+CREATE TABLE `hrbp_staffinfo_role` (
+  `employeeID` varchar(16) NOT NULL COMMENT '用户ID',
+  `role_id` int(10) NOT NULL COMMENT '系统角色ID',
+  PRIMARY KEY (`employeeID`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户和角色关联表';
+
+-- ----------------------------
+-- Records of hrbp_staffinfo_role
+-- ----------------------------
+INSERT INTO `hrbp_staffinfo_role` VALUES ('148550', '1');
+INSERT INTO `hrbp_staffinfo_role` VALUES ('148551', '2');
