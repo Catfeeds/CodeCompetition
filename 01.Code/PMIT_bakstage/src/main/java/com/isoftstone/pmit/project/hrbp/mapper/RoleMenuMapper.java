9588e56bbf7ce134c642fb4b42a97acee9f9a12b
dgjsjf <15289340589@163.com>
2018-11-29 17:09:12 +0800

    mapper

---
+++ 01.Code/PMIT_bakstage/src/main/java/com/isoftstone/pmit/project/hrbp/mapper/RoleMenuMapper.java
@@ -0,0 +1,19 @@
+package com.isoftstone.pmit.project.hrbp.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author lf
+ */
+@Mapper
+@Repository
+public interface RoleMenuMapper {
+
+    void deleteMenuByRid(@Param("roleId")Integer roleId);
+
+    int addMenu(@Param("roleId")Integer roleId, @Param("menuIds")Integer[] menuIds);
+
+    void deleteRole(@Param("roleId") Integer roleId);
+}
