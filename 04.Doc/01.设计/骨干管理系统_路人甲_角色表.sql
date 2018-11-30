7c56cf397229c9dfdaa51043984f4f6e10af1c1b
dgjsjf <15289340589@163.com>
2018-11-30 13:15:53 +0800

    更新角色表

--- 04.Doc/01.设计/骨干管理系统_路人甲_角色表.sql
+++ 04.Doc/01.设计/骨干管理系统_路人甲_角色表.sql
@@ -24,12 +24,7 @@ CREATE TABLE `hrbp_role` (
   `role_name` varchar(30) NOT NULL COMMENT '角色名称',
   `role_key` varchar(100) DEFAULT NULL COMMENT '角色权限字符串',
   `role_sort` int(4) DEFAULT NULL COMMENT '显示顺序',
-  `data_scope` int(4) DEFAULT '1' COMMENT '数据范围（1：全部数据权限 2：自定数据权限）',
-  `status` int(4) DEFAULT '1' COMMENT '角色状态（0正常 1停用）',
-  `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `update_by` varchar(64) DEFAULT NULL COMMENT '更新者',
-  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `description` varchar(500) DEFAULT NULL COMMENT '描述',
   `remark` varchar(500) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`role_id`)
@@ -38,7 +33,7 @@ CREATE TABLE `hrbp_role` (
 -- ----------------------------
 -- Records of hrbp_role
 -- ----------------------------
-INSERT INTO `hrbp_role` VALUES ('1', '管理员', 'admin', '1', '1', '1', 'admin', '2018-11-25 14:09:40', null, null, '系统设置', null);
-INSERT INTO `hrbp_role` VALUES ('2', '骨干员工', 'main', '1', '2', '1', 'admin', '2018-11-25 14:09:40', null, null, '系统维护', null);
-INSERT INTO `hrbp_role` VALUES ('3', '信息录入员', 'pm', '1', '3', '1', 'admin', '2018-11-25 14:09:40', null, null, '负责成绩导入', null);
-INSERT INTO `hrbp_role` VALUES ('4', 'guess', 'guess', '1', '4', '0', 'admin', '2018-11-28 17:23:14', null, null, '查看报表', null);
+INSERT INTO `hrbp_role` VALUES ('1', '管理员', 'admin', '1', '2018-11-25 14:09:40', '系统设置', null);
+INSERT INTO `hrbp_role` VALUES ('2', '骨干员工', 'main', '2', '2018-11-25 14:09:40', '系统维护', null);
+INSERT INTO `hrbp_role` VALUES ('3', '信息录入员', 'pm', '3', '2018-11-25 14:09:40', '负责成绩导入', null);
+INSERT INTO `hrbp_role` VALUES ('4', 'guess', 'guess', '4', '2018-11-28 17:23:14', '查看报表', null);
