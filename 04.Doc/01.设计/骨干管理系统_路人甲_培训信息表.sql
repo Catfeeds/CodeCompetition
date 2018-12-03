835f4ba4bcca8cddffd92af34c18535f3744371d
zhoaye358 <15929997031@139.com>
2018-12-03 20:29:24 +0800

    培训信息表_建表语句提交

---
+++ 04.Doc/01.设计/骨干管理系统_路人甲_培训信息表.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `training_info` (
+  `training_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '培训ID',
+  `training_name` varchar(20) DEFAULT NULL COMMENT '培训名称',
+  `series` varchar(20) DEFAULT NULL COMMENT '所属系列',
+  `sort` varchar(20) DEFAULT NULL COMMENT '所属体系',
+  `type` varchar(20) DEFAULT NULL COMMENT '所属类别',
+  `bu` varchar(20) DEFAULT NULL COMMENT '所属产品线',
+  `creator_id` varchar(20) DEFAULT NULL COMMENT '创建人工号',
+  `creator_name` varchar(20) DEFAULT NULL COMMENT '创建人名称',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `updater_id` varchar(20) DEFAULT NULL COMMENT '最后更新人工号',
+  `updater_name` varchar(20) DEFAULT NULL COMMENT '最后更新人名称',
+  `training_des` varchar(255) DEFAULT NULL COMMENT '培训简介',
+  `training_duration` varchar(20) DEFAULT NULL COMMENT '培训时长',
+  PRIMARY KEY (`training_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
