84370ad0fb28b2f2e0845f9c16b75c348efa8d75
dgjsjf <15289340589@163.com>
2018-12-13 16:53:38 +0800

    修改登录表

--- 04.Doc/01.设计/sql建表语句/骨干管理系统_路人甲_员工登录表.sql
+++ 04.Doc/01.设计/sql建表语句/骨干管理系统_路人甲_员工登录表.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50560
 File Encoding         : 65001
 
-Date: 2018-12-07 18:30:47
+Date: 2018-12-13 16:12:12
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,17 +20,16 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `login_information`;
 CREATE TABLE `login_information` (
-  `login_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '登陆id',
-  `employee_ID` varchar(16) DEFAULT NULL COMMENT '软通工号',
+  `employee_ID` varchar(16) NOT NULL DEFAULT '' COMMENT '软通工号',
   `employee_name` varchar(16) DEFAULT NULL COMMENT '员工姓名',
   `pass_word` varchar(32) DEFAULT NULL COMMENT '密码',
-  `role_id` varchar(16) DEFAULT NULL COMMENT '角色ID',
-  PRIMARY KEY (`login_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+  `role_id` int(10) DEFAULT NULL COMMENT '角色ID',
+  PRIMARY KEY (`employee_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of login_information
 -- ----------------------------
-INSERT INTO `login_information` VALUES ('1', '10008611', '张1博', 'e10adc3949ba59abbe56e057f20f883e', '1');
-INSERT INTO `login_information` VALUES ('2', '10008619', '杨1丽', 'e10adc3949ba59abbe56e057f20f883e', '2');
-INSERT INTO `login_information` VALUES ('3', '10008629', '陈D卿', 'e10adc3949ba59abbe56e057f20f883e', '3');
+INSERT INTO `login_information` VALUES ('10008611', '张1博', 'e10adc3949ba59abbe56e057f20f883e', '1');
+INSERT INTO `login_information` VALUES ('10008619', '杨1丽', 'e10adc3949ba59abbe56e057f20f883e', '2');
+INSERT INTO `login_information` VALUES ('10008629', '陈D卿', 'e10adc3949ba59abbe56e057f20f883e', '3');
